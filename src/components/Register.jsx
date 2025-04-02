4b766ca788f1354aae315fb8579286a875d71033
chethan3065 <chethankanni@gmail.com>
2025-04-02 11:45:27 +0530

    new

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,43 +1,35 @@
 import React from "react";
 import { useState } from "react";
-import "./Register.css"
-import { Link } from "react-router-dom";
+import "./Register.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { appContext } from "../App";
 export default function Register() {
-  const [users, setUsers] = useState([]);
-  const [user, setUser] = useState({});
+  const { user, setUser, users, setUsers } = useContext(appContext);
+  const [msg, setMsg] = useState();
+  const Navigate = useNavigate();
   const handleSubmit = () => {
-  //   const nameExists = users.some((u) => u.name === user.name);
-  //   const emailExists = users.some((u) => u.email === user.email);
-
-  //   if (nameExists) {
-  //     alert("Username already exists!");
-  //     return;
-  //   }
-
-  //   if (emailExists) {
-  //     alert("Email already registered!");
-  //     return;
-  //   }
-  //   setUsers([...users, user]);
-  //   setUser({}); 
-
-  const found = users.find((value) => value.email === user.email);
-  if(found) {
-    setMsg("User already exist")
-  }
+    const found = users.find((value) => value.email === user.email);
+    if (found) {
+      setMsg("User already exists");
+    } else {
+      setUsers([...users, user]);
+      setMsg();
+      Navigate("/");
+    }
   };
-  const handleDelete = (value) => {
-    setUsers(users.filter((element) => element !== value))
-  }
+  const handleDelete = (email) => {
+    setUsers(users.filter((value) => value.email != email));
+  };
   return (
     <div className="App-Register-Row">
       <div>
         <h2>Registration Form</h2>
+        {msg}
         <p>
           <input
             type="text"
             placeholder="Enter name"
-            value={user.name || ""}
             onChange={(e) => setUser({ ...user, name: e.target.value })}
           ></input>
         </p>
@@ -63,13 +55,13 @@ export default function Register() {
         </p>
       </div>
       <div>
-        {
-          users && users.map((value,index)=>(
-            <li key={index}>{value.name}-{value.email}-{value.password}
-            <button onClick={() => handleDelete(value)}>Delete</button>
+        {users &&
+          users.map((value, index) => (
+            <li>
+              {value.name}-{value.email}-{value.password}
+              <button onClick={() => handleDelete(value.email)}>Delete</button>
             </li>
-          ))
-        }
+          ))}
       </div>
     </div>
   );
